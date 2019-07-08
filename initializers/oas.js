ac957460a335fbecb9e7713b780d936a44e5c15a
hshpm <43015747+hshpm@users.noreply.github.com>
2019-07-08 02:07:52 +0200

    only run in test

--- initializers/oas.js
+++ initializers/oas.js
@@ -1,21 +1,23 @@
-'use strict'
+const { Initializer, api } = require("actionhero");
 
-const { Initializer, api } = require('actionhero')
-
-const Oas = require('../classes/oas')
+const Oas = require("../classes/oas");
 
 module.exports = class OasInitializer extends Initializer {
-  constructor () {
-    super()
-    this.name = 'oas'
+  constructor() {
+    super();
+    this.name = "oas";
   }
 
-  async initialize () {
-    api.oas = new Oas()
+  async initialize() {
+    if (api.env === "test") {
+      api.oas = new Oas();
+    }
   }
 
-  async start () {
-    await api.oas.buildOpenApiDocument()
-    await api.oas.writeOpenApiDocument()
+  async start() {
+    if (api.env === "test") {
+      await api.oas.buildOpenApiDocument();
+      await api.oas.writeOpenApiDocument();
+    }
   }
 }
